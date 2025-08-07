22e96eec46bc01753e6a951bef1cf1566c16117c
WANGICHEN <149982770+WANGICHEN@users.noreply.github.com>
2025-08-07 14:37:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ st.title("PDF → Word 自動轉換工具")
 pdf_file = st.file_uploader("請上傳 PDF 檔案", type=["pdf"])
 
 # 勾選要產出的格式
+certif = st.checkbox("CB with Certif.")
+
+# 勾選要產出的格式
 gma_filter = st.checkbox("產出 GMA Word")
 saa_filter = st.checkbox("產出 SAA Word")
 stcoa_filter = st.checkbox("產出 STCOA Word")
@@ -31,7 +34,7 @@ if pdf_file and (gma_filter or saa_filter or stcoa_filter):
                 word_output_name = pdf_file.name.replace(".pdf", f"_{name}.docx")
                 output_path = os.path.join(tmpdir, word_output_name)
 
-                doc = function.run(pdf_path, template)
+                doc = function.run(pdf_path, template, certif)
                 doc.save(output_path)
 
                 with open(output_path, "rb") as out_file:
@@ -41,3 +44,4 @@ if pdf_file and (gma_filter or saa_filter or stcoa_filter):
                         file_name=word_output_name,
                         mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
                     )
+
